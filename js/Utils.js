b870f3aa7f7f7dbd2613913a3d50315f1c7adf06
G43riko <gcsollei@hotmail.com>
2016-01-28 16:17:50 +0100

    Update Utils.js

--- js/Utils.js
+++ js/Utils.js
@@ -1,3 +1,15 @@
 function changeIfEqual(variable, value, newValue){
     return variable == value ? newValue : variable;
 }
+Array.prototype.contains = function(obj){
+    var i = this.length;
+    while(i--)
+        if(this[i] == obj)
+            return true;
+    return false;
+    
+    for(var i in this)
+        if(this[i] == obj)
+            return true;
+    return false;
+}
